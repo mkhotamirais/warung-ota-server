a47b1489b6d522dc4f391e15f2c79be93a2e2cd4
Mkhotamirais <mkhotamirais@gmail.com>
2024-05-28 00:19:22 +0700

    semoga akhir

--- app.js
+++ app.js
@@ -22,14 +22,11 @@ const allowedOrigins = [
   "http://localhost:5174",
 ];
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-
-app.use(cookieParser());
 app.use((req, res, next) => {
   if (allowedOrigins.includes(req.headers.origin)) res.header("Access-Control-Allow-Credentials", true);
   next();
 });
+
 const corsOptions = {
   origin: function (origin, callback) {
     allowedOrigins.indexOf(origin) !== -1 || !origin ? callback(null, true) : callback(new Error("Not allowed by CORS"));
@@ -37,6 +34,9 @@ const corsOptions = {
   optionsSuccessStatus: 200,
 };
 app.use(cors(corsOptions));
+app.use(cookieParser());
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
 app.get("/", (req, res) => {
   res.send(`welcome to warungota server`);
