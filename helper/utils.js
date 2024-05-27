c7ccaf3153b539ba0ca6a8910447f48d1273e823
Mkhotamirais <mkhotamirais@gmail.com>
2024-05-28 00:12:05 +0700

    berhasil

--- helper/utils.js
+++ helper/utils.js
@@ -25,7 +25,7 @@ const setCookie = (res, name, token) => {
   res.cookie(`${name}`, token, {
     secure: true,
     httpOnly: true,
-    maxAge: 30 * 24 * 60 * 60 * 1000,
+    // maxAge: 30 * 24 * 60 * 60 * 1000,
     sameSite: "Strict",
     // path: "/",
   });
